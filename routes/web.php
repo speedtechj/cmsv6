770fd1a3bc8a567484f6482468c40f1e2f4e43ad
speedtechj <arbiasjr@gmail.com>
2024-05-28 10:15:44 -0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BarcodeController;
+use App\Http\Controllers\InvoiceController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('{record}/barcode',[BarcodeController::class,'barcode'])->name('barcode1.pdf.download');
+Route::get('{record}/pdf',[InvoiceController::class,'invoice'])->name('barcode.pdf.download');
+
